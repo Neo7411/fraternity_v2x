dbc0dc4d6829960acf747a3f1d961ae01f4ca572
Angi David <angi.david@inf.unideb.hu>
2026-06-08 23:06:48 +0200

    ez komoly volt

--- scripts/run_cont.sh
+++ scripts/run_cont.sh
@@ -9,10 +9,14 @@ fi
 NAME_ARG="$1"
 NUMB="${NAME_ARG##*_}"  
 
+
+# a lenti s
+
 rocker --x11 --privileged --nvidia \
     --network=v2x_net  \
     --volume "$HOME/fraternity/VANETZA/fraternity_v2x/tools/:/home/aw/tools" \
     --volume "$HOME/fraternity/VANETZA/fraternity_v2x/v2x_modul/:/home/aw/dev" \
+    --volume "$HOME/fraternity/VANETZA/fraternity_v2x/vanetza/:/home/aw/vanetza_src" \
     --volume "$HOME/autoware_maps/:/home/aw/maps" \
     --name "$NAME_ARG" \
     --hostname "$NAME_ARG" \
